9d9fa7d1453dfa8aa960d2927b88d99061b6c0c5
Awie-Editor <lanasosi252@gmail.com>
2026-08-04 05:13:36 +0100

    animations.js

--- js/js/animations.js
+++ js/js/animations.js
@@ -1,1 +1,17 @@
+function fadeIn(element, delay = 0) {
 
+    element.style.opacity = "0";
+
+    element.style.transform = "translateY(30px)";
+
+    element.style.transition = `all 0.8s ease ${delay}s`;
+
+    setTimeout(() => {
+
+        element.style.opacity = "1";
+
+        element.style.transform = "translateY(0)";
+
+    }, 100);
+
+}
